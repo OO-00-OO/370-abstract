54fad82ae47c0ecae6518e653a14ad7e5b9780e3
BadiO <Omer.badi67@qmail.cuny.edu>
2022-08-07 12:57:04 -0400

    The Program works with abstract

---
+++ src/Dessert_Menu copy.java
@@ -0,0 +1,9 @@
+class Dessert_Menu extends Menu_Abstract {
+
+    public void Show_Dessert()
+    {
+        ShowMenu("Dessert_Menu.txt");
+    }
+
+}
+
---
+++ src/Dinner_Menu.java
@@ -0,0 +1,9 @@
+class Dinner_Menu extends Menu_Abstract {
+
+    public void Show_Dinner()
+    {
+        ShowMenu("Dinner_Menu.txt");
+    }
+
+}
+
